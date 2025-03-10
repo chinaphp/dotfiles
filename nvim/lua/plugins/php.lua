761329387a7948b0a9fc2572b7a50d90010fe263
7coffee <chinaphper@qq.com>
2025-03-10 17:20:17 +0800

    LICENSE

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -5,7 +5,7 @@ return {
       servers = {
         intelephense = {
           init_options = {
-            licenseKey = os.getenv("INTELEPHENSE_LICENCE"),
+            licenseKey = os.getenv("INTELEPHENSE_LICENSE"),
           },
           -- 可选：设置 PHP 版本（默认 8.1）
           settings = {
