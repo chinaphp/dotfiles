1b6874fcd69aea66a39e1bd74ab604b3dc2ef25f
7coffee <chinaphper@qq.com>
2025-03-10 16:38:41 +0800

    updating neovim to use lazyvim

--- nvim/lua/plugins/obsidian.lua
+++
@@ -1,9 +0,0 @@
-require("obsidian").setup({
-  workspaces = {
-    {
-      name = "Notes",
-      -- path = "/Users/jack/Obsidian/Notes",
-      path = "/Users/jack/Library/Mobile Documents/iCloud~md~obsidian/Documents/zz/Notes",
-    },
-  },
-})
---
+++ nvim/lua/plugins/php.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          init_options = {
+            licenseKey = os.getenv("INTELEPHENSE_LICENSE"),
+          },
+          -- 可选：设置 PHP 版本（默认 8.1）
+          settings = {
+            intelephense = {
+              environment = {
+                phpVersion = "7.4.33",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
