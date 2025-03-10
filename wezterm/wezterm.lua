a2b13703ba8e8711bdef66201cd4a6a652cf76d4
jack <jack@chinaphp.com>
2025-03-10 11:06:58 +0800

    wezterm font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,19 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 return {
 	adjust_window_size_when_changing_font_size = false,
 	-- color_scheme = 'termnial.sexy',
-	color_scheme = 'Catppuccin Mocha',
+	color_scheme = "Catppuccin Mocha",
 	enable_tab_bar = false,
 	font_size = 16.0,
-	font = wezterm.font('JetBrains Mono'),
+	-- font = wezterm.font('JetBrains Mono'),
+	font = wezterm.font_with_fallback({
+		"JetBrains Mono", -- 主字体（英文字体）
+		"PingFang SC", -- 中文字体
+		"Hiragino Sans GB", -- 备选字体（macOS 自带）
+	}),
 	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
-	
+
 	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
 	-- window_background_image_hsb = {
 	-- 	brightness = 0.01,
@@ -19,25 +24,25 @@ return {
 	window_background_opacity = 1.0,
 	-- window_background_opacity = 0.78,
 	-- window_background_opacity = 0.20,
-	window_decorations = 'RESIZE',
+	window_decorations = "RESIZE",
 	keys = {
 		{
-			key = 'q',
-			mods = 'CTRL',
+			key = "q",
+			mods = "CTRL",
 			action = wezterm.action.ToggleFullScreen,
 		},
 		{
-			key = '\'',
-			mods = 'CTRL',
-			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+			key = "'",
+			mods = "CTRL",
+			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
 		},
 	},
 	mouse_bindings = {
-	  -- Ctrl-click will open the link under the mouse cursor
-	  {
-	    event = { Up = { streak = 1, button = 'Left' } },
-	    mods = 'CTRL',
-	    action = wezterm.action.OpenLinkAtMouseCursor,
-	  },
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
 	},
 }
